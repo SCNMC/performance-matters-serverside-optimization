cf772cf96c7d856449948a339afbe9048f8a01e7
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-05-13 18:12:33 +0200

    avif images

--- main.js
+++ main.js
@@ -1,49 +1,53 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000;
+const compression = require('compression')
+const port = 3000
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-// Stel ejs in als template engine
-app.set('view engine', 'ejs')
-app.set('views', './views')
-
-
-// Stel een static map in
+//serve public files 
 app.use(express.static('public'))
 
-// Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello world!')
-  res.render('index', {
-    pageTitle: 'Performance Optimalisatie',
-  })
+// hook up template engine 
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(compression())
+n
+// Stel caching headers in
+app.use(function (req, res, next) {
+  if (req.method == 'GET') {
+    res.set('Cache-control', 'public, max-age=300')
+  } else {
+    res.set('Cache-control', `no-store`)
+  }
+  next()
 })
 
+   
 //fetch api project data//
 app.get('/', (req, res) => {
-  fetchJson("https://chipr.api.fdnd.nl/v1/projects").then(function (jsonData) {
-      res.render('index', {
-        title: 'Dit is de chippr api',
-        projects: jsonData.data,
+    fetchJson("https://chipr.api.fdnd.nl/v1/project").then(function (jsonData) {
+        res.render('index', {
+          title: 'Dit is de chippr api',
+          projects: jsonData.data,
+        })
       })
     })
-  })
 
-  // get detail page id//
+    // get detail page id//
 
-  app.get("/:id", (req, res) => {
-      fetchJson(`https://chipr.api.fdnd.nl/v1/project/${req.params.id}`).then(
-        function (jsonData) {
-          res.render("detail", {
-            project: jsonData.data[0],
-          });
-        }
-      );
-    });
+    app.get("/:id", (req, res) => {
+        fetchJson(`https://chipr.api.fdnd.nl/v1/project/${req.params.id}`).then(
+          function (jsonData) {
+            res.render("detail", {
+              project: jsonData.data[0],
+            });
+          }
+        );
+      });
 
-// Zwengel de server aan
-app.listen(process.env.PORT || 3000, () => console.log(`App avaialble on http://localhost:3000`))
+
+ app.listen(process.env.PORT || 3000, () => console.log(`App avaialble on http://localhost:3000`))
 
 /**
  * Wraps the fetch api and returns the response body parsed through json
@@ -51,7 +55,7 @@ app.listen(process.env.PORT || 3000, () => console.log(`App avaialble on http://
  * @returns the json response from the api endpoint
  */
  async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error)
-}
+    return await fetch(url)
+      .then((response) => response.json())
+      .catch((error) => error)
+  }
